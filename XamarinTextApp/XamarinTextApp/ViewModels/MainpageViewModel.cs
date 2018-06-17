815bf8b1a17267485e7cb9431657ee5ebca8992c
Rahul <37959182+rahulgitacc@users.noreply.github.com>
2018-06-17 16:44:34 +0530

    Added INotifyPropertyChanged and Command

--- XamarinTextApp/XamarinTextApp/ViewModels/MainpageViewModel.cs
+++ XamarinTextApp/XamarinTextApp/ViewModels/MainpageViewModel.cs
@@ -1,11 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using Xamarin.Forms;
 
 namespace XamarinTextApp.ViewModels
 {
-    class MainpageViewModel
+    // Inheritated INotifyPropertyChanged for 
+    class MainpageViewModel : INotifyPropertyChanged
     {
-        
+        private string labelText = "hello World!!";
+        public string LabelText {
+            get{
+                return labelText;
+            }
+            set {
+                labelText = value;
+                RaisePpropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //helper method
+        protected void RaisePpropertyChanged([CallerMemberName] string caller = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
+        }
+
+        private Command changeTextCommand;
+        public Command ChangeTextCommand
+        {
+            get
+            {
+                return changeTextCommand ?? (changeTextCommand = new Command(
+                    () =>
+                    {
+                        LabelText = "GoodBye";
+                    }));
+            }
+        }
     }
 }
